7724ba3bb797902b3b3ce96d0321e80e538c05a1
adnanjpg <adnanfahedhr@gmail.com>
2024-08-20 18:03:09 +0300

    bookdto refs

--- BookDto/BookDto.cs
+++ BookDto/BookDto.cs
@@ -6,5 +6,6 @@ public class BookDto
 {
     public Guid Id { get; set; }
     public string Title { get; set; }
-    public IPublisherDto Publisher { get; set; }
+    public Publisher.Publisher? Publisher { get; set; }
+    public Author? Author { get; set; }
 }
