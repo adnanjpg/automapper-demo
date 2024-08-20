bbff174880280346c2f12916eed682ec2a89088f
adnanjpg <adnanfahedhr@gmail.com>
2024-08-20 18:17:33 +0300

    fix publisher mapping

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using AutoMapDemo;
+using AutoMapDemo.Publisher;
 using AutoMapper;
 
 Console.WriteLine("Hello, World!");
 
 var mapper = new MapperConfiguration(cfg =>
 {
-    cfg.CreateMap<Book, BookDto>();
+    // Book only has IPublisher, while BookDto has Publisher and Author
+    // we want to insert either Publisher or Author into IPublisher
+    cfg.CreateMap<BookDto, Book>()
+        .ForMember(dest => dest.Publisher,
+            opt => opt.MapFrom(src => (IPublisher?)src.Publisher ?? src.Author))
+        ;
 }).CreateMapper();
+
+var Dto = new BookDto
+{
+    Id = Guid.NewGuid(), Title = "Book Title",
+    Publisher = new Publisher { Id = Guid.NewGuid(), Name = "Publisher Name", Founded = DateTime.Now }
+};
+
+var entity = mapper.Map<Book>(Dto);
+
+Console.WriteLine($"publisher id: {entity.Publisher.Id}");
--- obj/Debug/net8.0/AutoMapDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/AutoMapDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoMapDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7724ba3bb797902b3b3ce96d0321e80e538c05a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoMapDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoMapDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
